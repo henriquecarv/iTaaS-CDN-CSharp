77ac76fbaa24e2f32590e19138faeb45324678ba
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2018-08-04 16:21:20 -0300

    Initial commit

---
+++ iTaaS/ConsoleLog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Agile Content!");
+        }
+    }
+}
---
+++ run.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd iTaaS && dotnet run --project ConsoleLog/
